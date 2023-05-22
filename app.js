897103a522f691f90465c83f0bdee4f4dfe655d6
TomE <tommevans210@gmail.com>
2023-05-22 15:42:40 +0700

    all good

---
+++ app.js
@@ -0,0 +1,59 @@
+const gameBoard = document.querySelector('#gameboard');
+const playerDisplay = document.querySelector('#player');
+const infoDisplay = document.querySelector('#info-display');
+const width = 8
+
+const startPieces = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+];
+
+function createBoard() {
+  startPieces.forEach((startPiece) => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.innerHTML = startPiece;
+    square.classList.add('beige');
+    gameBoard.append(square);
+  });
+}
+
+createBoard();
